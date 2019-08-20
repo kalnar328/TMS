f7e2acb3570e6f5d6adb01946335e4badbe2353f
Kalna Ranasinghe <kalna.ranasinghe@ifsworld.com>
2019-08-20 14:35:51 +0530

    Modal added

--- tms-app/src/Views/editTrainer.component.js
+++ tms-app/src/Views/editTrainer.component.js
@@ -1,11 +1,11 @@
+
 import React, { Component } from 'react';
 import axios from 'axios';
 
-// //modal popuo dependency 
-// import Modal from '@trendmicro/react-modal';
-// // Be sure to include styles at some point, probably during your bootstraping
-// import '@trendmicro/react-modal/dist/react-modal.css'
+import '../css/trainer.css';
 
+import '@trendmicro/react-modal/dist/react-modal.css';
+import Modal from '@trendmicro/react-modal';
 
 export default class EditTrainer extends Component{
     constructor(props) {
@@ -15,11 +15,13 @@ export default class EditTrainer extends Component{
         this.onChangeName = this.onChangeName.bind(this);
         this.onChangeDesination = this.onChangeDesination.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+        // this.close = this.close.bind(this);
 
         this.state = {
             trainerId: '',
             trainerName: '',
             designation: ''
+            // isOpened: false
         };
     }
 
@@ -67,38 +69,54 @@ export default class EditTrainer extends Component{
         axios.put('http://localhost:4000/edit/'+this.props.match.params.trainerId, updateTrainer)
             .then(res => console.log(res.data))
             .then(data => {
-                alert('Trainer added sccesfully');
-                window.location.reload();
+                alert('Trainer updated sccesfully');
+                this.props.history.go(-1);
+                // window.location.reload();
             })
             .catch(function(error){
                 console.log(error);
             });;
     }
 
+    // close(){
+    //     this.setState({ modalIsOpen: false });
+    //   }
+
     render() {
+
         return (
-            <div>
 
-                <h3 align="center">Update Todo</h3>
+        <Modal>
+            <Modal.Header >
+                <Modal.Title>
+                    Edit Details
+                </Modal.Title>
+            </Modal.Header>
+        
+            <Modal.Body>
+                
                 <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label for="trainerId">ID</label>
-                        <input type="text" className="form-control" value = {this.state.trainerId} onChange={this.onChangeId} placeholder="Enter Trainer's Id"/>
-                    </div>
+                        <div className="form-group">
+                            <label for="trainerId">ID</label>
+                            <input type="text" className="form-control" value = {this.state.trainerId} onChange={this.onChangeId} placeholder="Enter Trainer's Id"/>
+                        </div>
 
-                    <div className="form-group">
-                        <label for="name">Full Name</label>
-                        <input type="text" className="form-control" value = {this.state.trainerName} onChange={this.onChangeName} placeholder="Enter full name"/>
-                    </div>
-                    
-                    <div className="form-group">
-                        <label for="Designation">Designation</label>
-                        <input type="text" className="form-control"value = {this.state.designation} onChange = {this.onChangeDesination} placeholder="Enter Designation"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                        <div className="form-group">
+                            <label for="name">Full Name</label>
+                            <input type="text" className="form-control" value = {this.state.trainerName} onChange={this.onChangeName} placeholder="Enter full name"/>
+                        </div>
+                        
+                        <div className="form-group">
+                            <label for="Designation">Designation</label>
+                            <input type="text" className="form-control"value = {this.state.designation} onChange = {this.onChangeDesination} placeholder="Enter Designation"/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Update</button>
+                        
                 </form>
+                <br></br>
+            </Modal.Body>
+        </Modal>
 
-            </div>
         )
     }
 }
